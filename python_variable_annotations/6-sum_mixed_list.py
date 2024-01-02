99b8e62b49cd03833ea4243d39c8040601d2ad44
Jean-Luc <jlucbilo@gmail.com>
2024-01-02 11:00:56 +0100

    a type-annotated function sum that returns a sum of several lists

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" a type-annotated function that
+returns the sum of several lists"""
+
+
+
+from typing import List, Union
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    return sum(mxd_lst)
