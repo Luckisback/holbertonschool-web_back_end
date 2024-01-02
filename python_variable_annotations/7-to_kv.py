76c3ad8818a46f415b59096888d6fcd8090b2e05
Jean-Luc <jlucbilo@gmail.com>
2024-01-02 11:29:11 +0100

    A type-annotated function that return a tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" A type-annotated function that returnsi
+a tuple, after computing some values"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    return (k, v**2)
