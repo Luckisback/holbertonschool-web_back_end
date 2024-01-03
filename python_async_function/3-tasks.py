10de91bbe8b3e2e54dd2063571535efa9bb72a67
Jean-Luc <jlucbilo@gmail.com>
2024-01-03 17:03:45 +0100

    fixing pycodestyle errors

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,5 +6,6 @@ module = importlib.import_module("0-basic_async_syntax")
 
 wait_random = module.wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     return asyncio.create_task(wait_random(max_delay))
