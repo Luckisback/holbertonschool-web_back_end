8d973c1dc8243141fa96ce7ba5d20b9b5fc54fcd
Jean-Luc <jlucbilo@gmail.com>
2024-01-03 16:15:01 +0100

    Executing several routine by calling some module from a packag

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,21 +1,13 @@
-#!/usr/bin/python3
-
-""" A function that import a module from a previous file
-and use a routine to display in sorted time"""
-
+#!/usr/bin/env python3
 
 import asyncio
 from typing import List
-from importlib import import_module
-wait_random = import_module('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n :int, max_delay : int) -> List[float]:
-    l = []
-    async def wait_and_append():
-        nonlocal l
-        for i in range(n):
-            a = await wait_random(max_delay)
-            l.append(a)
-    await asyncio.gather(*(wait_and_append() for _ in range(n)))
-    return sorted(l)
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    """ Sort the delays in ascending order """
+    sorted_delays = sorted(delays)
+    return sorted_delays
