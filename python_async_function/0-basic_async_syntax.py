ba5291b25f16c92e353fb9cfe8a42a85e7bc2306
Jean-Luc <jlucbilo@gmail.com>
2024-01-03 16:02:42 +0100

    A function that waits for a random number, waitss a moment and return it

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
+
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds.
+    Asynchronous coroutine that waits for a random
+    delay between 0 and max_delay seconds.
 
     Args:
-        max_delay (int): The maximum delay in seconds (default is 10).
+        max_delay (int): The maximum delay in seconds
+        (default is 10).
 
     Returns:
         float: The random delay.
