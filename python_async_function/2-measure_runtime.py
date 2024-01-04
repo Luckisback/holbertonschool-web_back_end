bda8bd1012c0e2ce1da644e33a761ce73afc179b
Jean-Luc <jlucbilo@gmail.com>
2024-01-04 12:45:57 +0100

    testing to overcome checker none-valaidate points

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-import asyncio
 import time
 from typing import List
-from concurrent_coroutines import wait_n
+import importlib
 
+wait_n = importlib.import_module('1-concurrent_coroutines').wait_n
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
-    await wait_n(n, max_delay)
+    result = wait_n(n, max_delay)
     end_time = time.time()
     total_time = end_time - start_time
-    average_time_per_execution = total_time / n
-    return average_time_per_execution
+    average_time_per_task = total_time / n
+    return average_time_per_task
