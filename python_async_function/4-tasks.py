fd24f4e4b2c02ef7b15665d274679df0e3ceee65
Jean-Luc <jlucbilo@gmail.com>
2024-01-04 12:03:41 +0100

    fixing pycodestyle errors

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/env python3
 
 import asyncio
 from typing import List
@@ -7,7 +6,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    delays = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
+    delays = await asyncio.gather(*(task_wait_random(max_delay)
+                                    for _ in range(n)))
     """ Sort the delays in ascending order """
     sorted_delays = sorted(delays)
     return sorted_delays
