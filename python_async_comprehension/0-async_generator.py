daa28878650762b40c6237172260f10f7eecf648
Jean-Luc <jlucbilo@gmail.com>
2024-01-05 11:52:11 +0100

    adding the comments in order to build documentation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,6 +6,8 @@ import random
 
 
 async def async_generator():
+    """ definition of the Asyncronous generator """
+    
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
