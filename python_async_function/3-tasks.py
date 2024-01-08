4dea9eb80e4af05af8851fec75d2db9330d6f85d
Jean-Luc <jlucbilo@gmail.com>
2024-01-08 22:42:15 +0100

    finxing the import instruction for the the module 'wait_random'

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
-
 """ Create a asyncio Task """
-
 import asyncio
-import importlib
-module = importlib.import_module("0-basic_async_syntax")
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-
-wait_random = module.wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """ return asyncio task """
