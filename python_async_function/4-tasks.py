a458fb89ee9de659d0360cb4455484d301e3c9ce
Jean-Luc <jlucbilo@gmail.com>
2024-01-08 22:39:38 +0100

    adding the missing comment and the 'loop for'

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-
+""" execute multiple coroutines at same time with async """
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    delays = await asyncio.gather(*(task_wait_random(max_delay)
-                                    for _ in range(n)))
-    """ Sort the delays in ascending order """
-    sorted_delays = sorted(delays)
-    return sorted_delays
+    """ call n times task_wait_random """
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+
+    for coroutine in asyncio.as_completed(coroutines):
+        delay = await coroutine
+        delays.append(delay)
+    return delays
