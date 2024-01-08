f951870ea9500983809278506ef323819a463a43
Jean-Luc <jlucbilo@gmail.com>
2024-01-08 22:38:18 +0100

    adding the missing comments

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+
+""" Create a asyncio Task """
+
 import asyncio
 import importlib
 module = importlib.import_module("0-basic_async_syntax")
@@ -6,6 +9,6 @@ module = importlib.import_module("0-basic_async_syntax")
 
 wait_random = module.wait_random
 
-
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ return asyncio task """
     return asyncio.create_task(wait_random(max_delay))
