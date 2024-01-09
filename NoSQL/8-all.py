4a6b4d78a4854057f9e2db0c18e552dacbcd7456
Jean-Luc <jlucbilo@gmail.com>
2024-01-09 17:59:29 +0100

    A script that returns a list of documents from a collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/en python3
+
+""" A function that lists all document in a collection"""
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    """ Defintion of the function"""
+    if not db.mongo_collection.count()
+        return []
+    return list(db.mongo_collection.find())
