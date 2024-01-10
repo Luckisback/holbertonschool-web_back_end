8bb785dac1250ad3f47100bfe4bca84071fe199f
Jean-Luc <jlucbilo@gmail.com>
2024-01-11 00:37:08 +0100

    adding the the instruction 'import pymongo'

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,6 +3,8 @@
 """A Python function that changes
 all topics based on the name"""
 
+import pymongo
+
 
 def update_topics(mongo_collection, name, topics):
     """ Receiving name whitch will concerned by the change"""
