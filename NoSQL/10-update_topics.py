29f6126ef9327d98efca440dfd57dcc637738730
Jean-Luc <jlucbilo@gmail.com>
2024-01-11 00:31:38 +0100

    A function that changes all topics based on the name

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""A Python function that changes
+all topics based on the name"""
+
+def update_topics(mongo_collection, name, topics):
+    """ Receiving name whitch will concerned by the change"""
+    filter_query = {"name": name}
+    update_data = {"$set": {"topics": topics}}
+    mongo_collection.update_one(filter_query, update_data)
