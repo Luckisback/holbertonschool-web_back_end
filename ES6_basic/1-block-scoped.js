eda3f0b38766af602836347819099cf1b35f68dc
Jean-Luc <jlucbilo@gmail.com>
2024-01-16 09:24:55 +0100

    fixing the seconde ideclaration of variable inside the 'if bloc'

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
-  const task = true;
-  const task2 = false;
+  const task = false;
+  const task2 = true;
   
   return [task, task2];
 }
